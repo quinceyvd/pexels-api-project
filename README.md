a58f1f4f3ab9a3d3a7599d821a6aebe9c5020419
quinceyvd <90757119+quinceyvd@users.noreply.github.com>
2023-01-23 23:25:29 +0100

    New landing page

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Pexels API project
-This is a front-end project I made to practice my JS skills using the Fetch API, and to solidify my CSS skills by creating a responsive design. The web app lets users up- or downvote a picture. The upvoted/liked pictures get stored into a collection users can revisit later. The images are provided by [Pexels' Image & Video API](https://www.pexels.com/api/).
+This is a front-end project I made to practice my JS skills using the Fetch API, and to solidify my CSS skills by creating a responsive design. The web app lets users "like"  or "downvote" a picture. The "liked" pictures get stored into a collection users can revisit later. The images are provided by [Pexels' Image & Video API](https://www.pexels.com/api/).
 
 The images get saved to the user's `localStorage`, so there are no back-end techniques used to keep track of liked photos. The Collection page later retrieves the data from localStorage and displays the saved pictures on the webpage.
 
 ### Setup
 The webapp can be launched by running the project on a local webserver. I personally used both [XAMPP](https://www.apachefriends.org/) and [VS Code Live Server](https://ritwickdey.github.io/vscode-live-server/) during development. 
 
-Before entering the webpage, the placeholder value for the API key must be replaced with a real Pexels API key, otherwise the script has no images to display. The placeholder value is stored on line 3 of [`index.js`](index.js#L3). Change the string's value from `"PEXELS_API_KEY"` to a string containing an actual key.
-
 ### How to use
+#### Landing page
+Upon entering the webpage, the user is prompted to insert an API key. Without a valid API key, the user won't be able to use the functionalities.
+
+![Landing page screenshot](/screenshots/landing-page-screenshot.png)
+
+
 #### Explore page
-Upon entering the webpage, the user is presented with a card featuring an image, along with a "Like" and "Downvote" button. By clicking the "Like" button, the image gets saved to the "Collection" page. The page then automatically refreshes, displaying a new image for the user to evaluate. If the "Downvote" button is clicked, the page similarly refreshes, presenting the user with a new image to view.
+After a Pexels API key is entered, he user is presented with a card featuring an image, along with a "Like" and "Downvote" button. By clicking the "Like" button, the image gets saved to the "Collection" page. The page then automatically refreshes, displaying a new image for the user to evaluate. If the "Downvote" button is clicked, the page similarly refreshes, presenting the user with a new image to view.
 
 ![Explore page screenshot](/screenshots/explore-page-screenshot.png)
 
---
+++ explore.js
@@ -0,0 +1,96 @@
+// API fetch preparations
+const query = "City";
+const apiKey = localStorage.getItem("api-key");
+const randPageNum = Math.floor(Math.random() * 995) + 1;
+
+// Fetch API data from Pexels
+const getData = async () => {
+    const data = await fetch(`https://api.pexels.com/v1/search?query=${query}&page=${randPageNum}&per_page=1`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: apiKey,
+        },
+    });
+    const response = await data.json();
+
+    if (!response.photos) {
+        console.log("No results found");
+        let errorDiv = document.createElement("div");
+        errorDiv.style.width = "100vw";
+        errorDiv.style.height = "100vh";
+        errorDiv.style.position = "absolute";
+        errorDiv.style.top = "0";
+        errorDiv.style.left = "0";
+        errorDiv.style.backgroundColor = "rgb(22, 22, 22)";
+        errorDiv.style.paddingTop = "200px";
+
+        let par = document.createElement("p");
+        par.style.textAlign = "center";
+        par.style.color = "rgb(202, 30, 30)";
+        let message = document.createTextNode("Images could not be loaded");
+        par.appendChild(message);
+
+        let par2 = document.createElement("p");
+        par2.style.textAlign = "center";
+        par2.style.color = "white";
+        let message2 = document.createTextNode("Please re-enter your  API key");
+        par2.appendChild(message2);
+
+        let link = document.createElement("a");
+        link.href = "index.html";
+        link.style.marginTop = "50px";
+        link.style.display = "block";
+        link.style.textAlign = "center";
+        link.style.color = "white";
+        let message3 = document.createTextNode("← Back");
+        link.appendChild(message3);
+        
+
+        errorDiv.appendChild(par);
+        errorDiv.appendChild(par2);
+        errorDiv.appendChild(link);
+        document.body.appendChild(errorDiv);
+
+    } else {
+
+        let photoData = [];
+        (response.photos).forEach(photo => {
+            photoData.push({
+                url: photo.src.large,
+                photographer: photo.photographer,
+                photographer_url: photo.photographer_url,
+                category: photo.alt,
+                id: photo.id.toString(),
+            });
+        });
+
+        // Modify HTML placeholders
+        document.getElementById('img').src = photoData[0].url;
+        document.getElementById("name").innerText = photoData[0].photographer.toUpperCase();
+        document.getElementById("name").href = photoData[0].photographer_url;
+        document.getElementById("description").innerText = photoData[0].category;
+
+        const favoriteBtn = document.getElementById("favoriteBtn");
+        favoriteBtn.addEventListener("click", () => {
+            // Saving photoData to localStorage
+            let favoriteData = [];
+            let favoriteObj = {
+                url: photoData[0].url,
+                photographer: photoData[0].photographer,
+                photographer_url: photoData[0].photographer_url,
+                category: photoData[0].category,
+                id: photoData[0].id,
+            };
+            favoriteData.push(JSON.stringify(favoriteObj));
+            localStorage.setItem(photoData[0].id, JSON.stringify(favoriteData));
+        });
+    }
+};
+
+getData(query);
+
+// Refresh when clicking downvote button
+const downvoteBtn = document.getElementById("downvoteBtn");
+downvoteBtn.addEventListener("click", () => {
+});
